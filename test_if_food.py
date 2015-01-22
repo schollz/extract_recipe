f1767b63e21af248e867eac3acec43ac957aac86
Zack Scholl <zack.scholl@gmail.com>
2015-01-22 08:43:59 -0500

    Detects measurement and food words
    Former-commit-id: 035b7a17bc50b682feb29c8b50c89dc1713ca8d9

--- test_if_food.py
+++ test_if_food.py
@@ -1,13 +1,31 @@
 from nltk.corpus import wordnet
 import sys
+from pattern.en import singularize
+import string
+exclude = set(string.punctuation)
 
-synsets = wordnet.synsets(sys.argv[1])
+for word in sys.argv[1:]:
+  word = ''.join(ch for ch in word if ch not in exclude)
+  word = word.lower()
+  word = singularize(word)
+  synsets = wordnet.synsets(word)
+  foundLexname = False
+  for synset in synsets:
+    if 'food' in synset.lexname:
+      print 'food ',
+      foundLexname = True
+    if 'quantity' in synset.lexname:
+      print 'quantity ',
+      foundLexname = True
+  if not foundLexname:
+    print 'none ',
+    '''
+    print "-" * 10
+    print "Name:", synset.name
+    print "Lexical Type:", synset.lexname
+    print "Lemmas:", synset.lemma_names
+    print "Definition:", synset.definition
+    for example in synset.examples:
+      print "Example:", example
+    ''' 
 
-for synset in synsets:
-  print "-" * 10
-  print "Name:", synset.name
-  print "Lexical Type:", synset.lexname
-  print "Lemmas:", synset.lemma_names
-  print "Definition:", synset.definition
-  for example in synset.examples:
-    print "Example:", example
