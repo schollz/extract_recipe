7e363c4d08fe782766324ebbbc61c1ea579aed50
Zack Scholl <zack.scholl@gmail.com>
2015-01-22 10:45:03 -0500

    Extracts foods, measurements from ingredient list
    Former-commit-id: a9276816d1a6d076361324626c2a7f8003b0f942

--- test_if_food.py
+++ test_if_food.py
@@ -4,28 +4,54 @@ from pattern.en import singularize
 import string
 exclude = set(string.punctuation)
 
-for word in sys.argv[1:]:
+words = sys.argv[1:]
+index = 0
+done = False
+for word in words:
   word = ''.join(ch for ch in word if ch not in exclude)
   word = word.lower()
   word = singularize(word)
   synsets = wordnet.synsets(word)
-  foundLexname = False
   for synset in synsets:
-    if 'food' in synset.lexname:
-      print 'food ',
-      foundLexname = True
     if 'quantity' in synset.lexname:
-      print 'quantity ',
-      foundLexname = True
-  if not foundLexname:
-    print 'none ',
-    '''
-    print "-" * 10
-    print "Name:", synset.name
-    print "Lexical Type:", synset.lexname
-    print "Lemmas:", synset.lemma_names
-    print "Definition:", synset.definition
-    for example in synset.examples:
-      print "Example:", example
-    ''' 
+      done = True
+  if done:
+    break
+  index = index + 1
+
+quantities = [words[index],words[index+1]]
+print quantities
+
+words = words[2:]
+indices = []
+index = 0
+done = False
+for word in words:
+  word = ''.join(ch for ch in word if ch not in exclude)
+  word = word.lower()
+  word = singularize(word)
+  synsets = wordnet.synsets(word)
+  if len(synsets)<4:
+    for synset in synsets:
+      '''print "-" * 10
+      print "Name:", synset.name
+      print "Lexical Type:", synset.lexname
+      print "Lemmas:", synset.lemma_names
+      print "Definition:", synset.definition
+      for example in synset.examples:
+        print "Example:", example'''
+      if 'food' in synset.lexname:
+        indices.append(index)
+        break
+  index = index + 1
+
+foods = indices
+for index in indices:
+  print [words[index]]
+  if index > 0:
+    print [words[index-1],words[index]]
+  if index < len(words)-1:
+    print [words[index],words[index+1]]
+  
+
 
