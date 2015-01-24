e2ef0e1f87d1fbe7ead916df56286a263efc35c0
Zack Scholl <zack.scholl@gmail.com>
2015-01-23 22:23:54 -0500

    Changed web interface to simple AJAX.
    Former-commit-id: bc99ddc0ea1eab129a0e8d4e8216f5a86f3c1642

---
+++ server.py
@@ -0,0 +1,37 @@
+import threading
+import webbrowser
+import BaseHTTPServer
+import SimpleHTTPServer
+from extract_recipe import * 
+
+FILE = 'frontend.html'
+PORT = 8081
+
+
+class TestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """The test example handler."""
+
+    def do_POST(self):
+        """Handle a post request by returning the square of the number."""
+        length = int(self.headers.getheader('content-length'))        
+        data_string = self.rfile.read(length)
+        result = extract_recipe_main(data_string).replace('\n','<br>')
+        self.wfile.write(result)
+
+
+def open_browser():
+    """Start a browser after waiting for half a second."""
+    def _open_browser():
+        webbrowser.open('http://localhost:%s/%s' % (PORT, FILE))
+    thread = threading.Timer(0.5, _open_browser)
+    thread.start()
+
+def start_server():
+    """Start the server."""
+    server_address = ("", PORT)
+    server = BaseHTTPServer.HTTPServer(server_address, TestHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    open_browser()
+    start_server()
