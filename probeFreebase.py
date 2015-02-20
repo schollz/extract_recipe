44f8522db34943e3ef3a8d5bebbb0fad6d6a10f3
Zack Scholl <zack.scholl@gmail.com>
2015-02-19 23:10:30 -0500

    Finished probeFreebase
    Former-commit-id: 0737870211a10eace223b0924f37ff2b9d9c73cc

--- probeFreebase.py
+++ probeFreebase.py
@@ -1,28 +1,33 @@
 import json
 import urllib
 import sqlite3 as lite
+from unidecode import unidecode
+
+last_ndb_no = '10909'
+start = False
 con = lite.connect('db')
 with con:    
   cur = con.cursor()    
-  cur.execute('select ndb_no from food_des')
+  cur.execute('select ndb_no,shrt_desc from food_des')
   rows = cur.fetchall()
   for row in rows:
     ndb_no =  row[0]
-
-    api_key = open(".freebase_api_key").read()
-    service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
-    query = [{"name": [],"/food/food/usda_id": ndb_no}]
-    params = {
-            'query': json.dumps(query),
-            'key': api_key
-    }
-    url = service_url + '?' + urllib.urlencode(params)
-    urlOpen = urllib.urlopen(url)
-    if urlOpen.code == 200:
+    print ndb_no
+    if start:
+      api_key = open(".freebase_api_key").read()
+      service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
+      query = [{"name": [],"/food/food/usda_id": ndb_no}]
+      params = {
+              'query': json.dumps(query),
+              'key': api_key
+      }
+      url = service_url + '?' + urllib.urlencode(params)
+      urlOpen = urllib.urlopen(url)
       response = json.loads(urlOpen.read())
       if len(response['result'])>0:
         for planet in response['result']:
+          print row[1] + unidecode(planet['name'][0])
           with open('ndb_no_freebase.txt','a') as f:
-            f.write(ndb_no + "|" + ','.join(planet['name']) + "\n")
-    else:
-      print "ERROR ON " + ndb_no
+            f.write(ndb_no + "|" + unidecode(planet['name'][0]) + "\n")
+    if last_ndb_no in ndb_no:
+      start = True
